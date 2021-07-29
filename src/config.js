f179c12b5568ae9c0bdb8ab3dcac521661ba82ea
Anirudh Aima <anirudh.aima@gmail.com>
2021-07-29 11:33:52 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     appId:'b980da64-ea7c-4499-a2f1-4b5ecb429ce9',
-    redirectUri:'http://localhost:3000',
+    redirectUri:'https://master.d2z398coqb0484.amplifyapp.com/',
     scopes:['user.read'],
     authority:'https://login.microsoftonline.com/consumers'
 }
